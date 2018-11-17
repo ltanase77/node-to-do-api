d6a5ae94c10ac3195e97b186b07060a4d391803b
Lucian Tanase <office@easyconsult.ro>
2018-11-17 21:47:01 +0200

    add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,42 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose.js');
+const {Todo} = require('./../server/models/todo.js');
+const {User} = require('./../server/models/users.js');
+
+var id = '5bf066c0872bd217f4cbcef3';
+
+if (!ObjectID.isValid(id)) {
+    console.log('ID not valid!');
+}
+
+Todo.find({
+    _id: id
+}).then((todos) => {
+    console.log('Todos', todos)
+})
+
+Todo.findOne({
+    _id: id
+}).then((todo) => {
+    console.log('Todo:', todo)
+});
+
+Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return console.log("Id not found!");
+    }
+    console.log('Todo by Id', todo)
+}).catch((err) => {
+    console.log('Error: ', err);
+});
+var user_id = '5be85194d46f2005ec34c5e8';
+
+User.findById(user_id).then( (user) => {
+    if (!user) {
+        return console.log("User not found!");
+    } else {
+        console.log ("User is: ", user);
+    }
+}).catch((err) => {
+    console.log("User ID is not valid: ", err);
+})
