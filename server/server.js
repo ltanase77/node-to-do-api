3cb110336b15bb48ceb9c1be54ce210fab4be38c
Lucian Tanase <office@easyconsult.ro>
2018-11-17 20:40:32 +0200

    adding get todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', (request, response) => {
     })
 });
 
+app.get('/todos', (request, response) => {
+    Todo.find().then((todos) => {
+        response.send({todos})
+    }).catch( (err) => {
+        response.status(400).send(err);
+    })
+})
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
