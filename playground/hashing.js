e0606053f6e5186a234600fe1db4fcc9ad37e11c
Lucian Tanase <office@easyconsult.ro>
2018-12-08 18:54:34 +0200

    Hash user password before you save it

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,28 @@
 const crypto = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
+let password = "zamoldegikos234";
+/* bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+}); */
+
+let hashedPassword = '$2a$10$OogDCb4lt0LSmuve2S51fey.xP8nxgM/T1h836CSjGLpDp9E6UKs2';
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+});
+
+/* const data = {
     id: 10,
 };
 
 const token = jwt.sign(data, '123abc');
 console.log(token);
 const decoded = jwt.verify(token, '123abc');
-console.log('decoded:', decoded);
+console.log('decoded:', decoded); */
 /* const message = 'I am user number three';
 const hash = crypto.SHA256(message).toString();
 
--- server/models/users.js
+++ server/models/users.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -70,6 +71,20 @@ UserSchema.statics.findByToken = function(token) {
 
     });
 };
+
+UserSchema.pre('save', function(next) {
+    const User = this;
+    if (User.isModified('password')) {
+        bcrypt.genSalt(14, (err, salt) => {
+            bcrypt.hash(User.password, salt, (err, hash) => {
+                User.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 const User = mongoose.model('User', UserSchema);
 
