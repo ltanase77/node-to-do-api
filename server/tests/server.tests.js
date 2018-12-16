5604bda99c8c5aa16b67430dbabf9efd4a3c7bf1
Lucian Tanase <office@easyconsult.ro>
2018-12-16 20:16:31 +0200

    Adding DELETE /users/me/token route test

--- server/tests/server.tests.js
+++ server/tests/server.tests.js
@@ -299,4 +299,24 @@ describe('POST /users/login', function() {
                 });
             });
     });
-})
+});
+
+describe('DELETE /users/me/token', function() {
+    it("should remove auth token on logout", function(done) {
+        supertest(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((error, response) => {
+                if (error) {
+                    return done(error);
+                }
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((error) => {
+                    done(error);
+                });
+            });
+    });
+});
